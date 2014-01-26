834b983193fecba355a6c74ad14f327672e1c208
lemuston <laura.mustonen@cs.helsinki.fi>
2014-01-26 19:13:40 +0200

    muutoksia

--- ratebeer/db/schema.rb
+++ ratebeer/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140119180652) do
+ActiveRecord::Schema.define(version: 20140119203104) do
 
   create_table "beers", force: true do |t|
     t.string   "name"
