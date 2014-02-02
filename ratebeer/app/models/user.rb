bd462ac1a8d8d39e705784d97098f5efed640746
lemuston <laura.mustonen@cs.helsinki.fi>
2014-02-02 22:27:27 +0200

    tehtävä3 yritys

--- ratebeer/app/models/user.rb
+++ ratebeer/app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ActiveRecord::Base
+  has_many :ratings   # käyttäjällä on monta ratingia
+end
+
+class Rating < ActiveRecord::Base
+  belongs_to :beer
+  belongs_to :user   # rating kuuluu myös käyttäjään
+
+  def to_s
+    "#{beer.name} #{score}"
+  end
 end
